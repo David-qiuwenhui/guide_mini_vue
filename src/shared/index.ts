cd655f2d5a2151c9118f3c12bbc17ee2407272f8
qiuwenhui <1059713136@qq.com>
2024-11-22 00:10:01 +0800

    ✨ feat: 实现组件 emit 功能

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -11,3 +11,20 @@ export const hasChange = (newValue, value) => {
 export const hasOwn = (val, key) => {
   return Object.prototype.hasOwnProperty.call(val, key);
 };
+
+// TPP开发思想: 先去写一个特定的行为 再重构成通用的行为
+// add -> Add
+// add-foo -> addFoo
+export const camelize = (str: string) => {
+  return str.replace(/-(\w)/g, (_, c: string) => {
+    return c ? c.toUpperCase() : "";
+  });
+};
+
+export const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const toHandlerKey = (str: string) => {
+  return str ? "on" + capitalize(str) : "";
+};
